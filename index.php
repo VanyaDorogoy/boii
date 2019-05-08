70b1729abe15dfb6ec2e82b6498ddd666b56fccf
HANG SARAH <631803@cart.ads>
2019-05-08 10:11:29 -0700

    more files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <?php include 'navbar.php'
+    ?>
+    
+    <div class="parallax"></div>
+    
+   <div class="flexp1">
+         <p>Hmong-American Harvest Church (aka Harvest Church) is a Christian church founded in 2007 by four families who envisioned a church culture based on biblical foundations that would suit the next generation without neglecting previous generations. </p>
+    </div>
+    
+   <div class="flexp2">
+         <p>We began meeting and worshipping in our homes like that of the apostolic times going from house to house. Though we were small, we loved God and remained faithful to serving Him and soon found a church to hold our weekly celebration service.</p>
+    </div>
+    
+    <?php include 'footer.php'
+    ?>
+</body>
+</html>
