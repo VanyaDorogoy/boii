cc44160880879b833d3c5ee6c443cbe45a7ea787
HERR ROSE <150065440@cart.ads>
2019-05-09 09:02:35 -0700

    renamed the leaders file to leaderss

---
+++ leaderss.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <?php include 'navbar.php'
+    ?>
+    
+    <div class="parallax"></div>
+    
+   <div class="flex1">
+         <div class="flexp1">
+             <p>Hmong-American Harvest Church (aka Harvest Church) is a Christian church founded in 2007 by four families who envisioned a church culture based on biblical foundations that would suit the next generation without neglecting previous generations. </p>
+        </div>
+        
+        <div class="flexp2">
+             <p>We began meeting and worshipping in our homes like that of the apostolic times going from house to house. Though we were small, we loved God and remained faithful to serving Him and soon found a church to hold our weekly celebration service.</p>
+        </div>
+    </div>
+    
+<div class="flex2">
+        <div class="flexmap">
+            
+        </div>
+        
+        <div class="flexservice">
+            <h2>Sunday School</h2>
+            <p> 2050 N Winery Ave 
+                #101, Fresno, CA 93703
+                <br>
+                Sunday School: 10:00 - 11:00 AM 
+                Worship Service: 11:30 - 12:30 PM
+                <br>
+                Nursery, Children Sunday School, and Worship
+            </p>
+        </div>
+    </div>
+    
+    
+    <?php include 'footer.php'
+    ?>
+    
+</body>
+</html>
