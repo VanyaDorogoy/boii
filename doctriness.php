5e691a574bd4728357d9e3652aab7d41fd7a48b0
HERR ROSE <150065440@cart.ads>
2019-05-10 10:07:27 -0700

    added placeholder images and finished the doctriness page. Started the leaderss page and finished the elders and the secretary.

--- doctriness.php
+++ doctriness.php
@@ -25,55 +25,55 @@
             
                 <br><br>
 
-            We believe Jesus Christ is the true God and the true man.
+            <b>We believe</b> Jesus Christ is the true God and the true man.
             
                 <br><br>
 
-            We believe he died upon the cross, the just for the unjust, as a substitutionary sacrifice, and all who believe in Him are justified on the ground of His shed blood.
+            <b>We believe</b> he died upon the cross, the just for the unjust, as a substitutionary sacrifice, and all who believe in Him are justified on the ground of His shed blood.
             
                 <br><br>
 
-            We believe he will come again to establish His kingdom, righteousness and peace. 
+            <b>We believe</b> he will come again to establish His kingdom, righteousness and peace. 
             
                 <br><br>
 
-            We believe the Holy Spirit is a divine person, sent to indwell, guide, teach, empower the believer, and convince the world of sin, of righteousness, and of judgment.
+            <b>We believe</b> the Holy Spirit is a divine person, sent to indwell, guide, teach, empower the believer, and convince the world of sin, of righteousness, and of judgment.
             
                 <br><br>
 
-            We believe the Old and New Testaments, inerrant as originally given, were verbally inspired by God and are a complete revelation of His will for the salvation of men.
+            <b>We believe</b> the Old and New Testaments, inerrant as originally given, were verbally inspired by God and are a complete revelation of His will for the salvation of men.
             
                 <br><br>
 
-            We believe all men are born with a sinful nature, are separated from the life of God, and can be saved only through the atoning work of the Lord Jesus Christ.
+            <b>We believe</b> all men are born with a sinful nature, are separated from the life of God, and can be saved only through the atoning work of the Lord Jesus Christ.
             
                 <br><br>
 
-            We believe the portion of the unrepentant and unbelieving is existence forever in conscious torment; and that of the believer, in everlasting joy and bliss.
+            <b>We believe</b> the portion of the unrepentant and unbelieving is existence forever in conscious torment; and that of the believer, in everlasting joy and bliss.
             
                 <br><br>
 
-            We believe salvation has been provided through Jesus Christ for all men; and  those who repent and believe in Him are born again of the Holy Spirit,  receive the gift of eternal life, and become the children of God.
+            <b>We believe</b> salvation has been provided through Jesus Christ for all men; and  those who repent and believe in Him are born again of the Holy Spirit,  receive the gift of eternal life, and become the children of God.
             
                 <br><br>
 
-            We believe it is the will of God that each believer should be filled with the Holy Spirit and be sanctified wholly, being separated from sin and the world and fully dedicated to the will  of God, thereby receiving power for holy living and effective service.
+            <b>We believe</b> it is the will of God that each believer should be filled with the Holy Spirit and be sanctified wholly, being separated from sin and the world and fully dedicated to the will  of God, thereby receiving power for holy living and effective service.
+            
+                <br><br>
+ 
+            <b>We believe</b> provision is made in the redemptive work of the Lord Jesus Christ for the healing of the mortal body.
             
                 <br><br>
 
-            We believe provision is made in the redemptive work of the Lord Jesus Christ for the healing of the mortal body.
+            <b>We believe</b> the Church consists of all those who believe on the Lord Jesus  Christ, are redeemed through His blood, and are born again of the Holy  Spirit. 
             
                 <br><br>
 
-            We believe the Church consists of all those who believe on the Lord Jesus  Christ, are redeemed through His blood, and are born again of the Holy  Spirit. 
+            <b>We believe</b> the local church is a body of believers in Christ who are joined  together for the worship of God, for edification through the Word of  God, for prayer, fellowship, the proclamation of the gospel, and  observance of the ordinances of Baptism and the Lord’s Supper.
             
                 <br><br>
 
-            We believe the local church is a body of believers in Christ who are joined  together for the worship of God, for edification through the Word of  God, for prayer, fellowship, the proclamation of the gospel, and  observance of the ordinances of Baptism and the Lord’s Supper.
-            
-                <br><br>
-
-            We believe there shall be a bodily resurrection of the just and of the unjust; for the former, a resurrection unto life: for the latter, a resurrection unto judgment.
+            <b>We believe</b> there shall be a bodily resurrection of the just and of the unjust; for the former, a resurrection unto life: for the latter, a resurrection unto judgment.
             
         </div>
         
--- leaderss.php
+++ leaderss.php
@@ -3,12 +3,115 @@
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
+    <link rel="stylesheet" href="csss.css">
 </head>
 <body>
    
 <?php include 'navbarr.php'?>
     
+    <div class="govbord">
+        
+        The Governing Board
+        
+    </div>
     
+    <div class="aye">
+    
+    <div class="flex">
+        
+        <img src="imagess/img1.jpg" alt="catto" class="catto">
+        
+        <div class="flex2">
+        
+        <div class="name">Neng Yang &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Elder</div>
+        
+        <div class="neng">
+            
+            Shepards the church to continue on with the mission and vision of the church.
+            
+            <br><br>
+            
+            (559) 476 - 3209
+            
+            <br><br>
+            
+            nengyang75@yahoo.com
+            
+            </div>
+            
+        </div>
+   
+    </div>
+    
+    </div>
+    
+<div class="aye">
+    
+    <div class="kou">
+        
+        <img src="imagess/img2.jpg" alt="catto" class="catto">
+        
+        <div class="flex2">
+        
+        <div class="name1">
+        
+        Kou Hang &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Elder
+        
+        </div>
+        
+        <div class="kou1">
+            
+            Manages the church of God, prays for members of the congregation, and supports staff.
+            
+            <br><br>
+            
+            (559) 476 - 3209
+            
+            <br><br>
+            
+            nengyang75@yahoo.com
+            
+            </div>
+            
+        </div>
+   
+    </div>
+    
+    </div>
+    
+    <div class="aye">
+        
+        <div class="flexy">
+           
+           <div class="ayo">
+               
+               <div class="name2">
+                   
+                   Youa Moua &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Secretary
+                   
+               </div>
+               
+               <div class="desp">
+                   
+                   Assists pastors and staff with administrative duties.
+                   
+                   <br><br>
+                   
+                   (559) 473 - 3529
+                   
+                   <br><br>
+                   
+                   moua09youa@icloud.com
+                   
+               </div>
+               
+           </div>
+            
+            <img src="imagess/img3.jpg" alt="kitty" class="cato">
+            
+        </div>
+        
+    </div>
     
 <?php include 'footerr.php'?>
     
